30fa2da5faab9f0a3cf30bd66ffbe7a04cc925dd
sridhram <51109498+sridhram@users.noreply.github.com>
2023-05-14 22:18:24 +0530

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/articles">Articles</Link>
+      </nav>
+      <h2>Logo</h2>
+      <nav>
+        <Link href="/" target={'_blank'}>
+          Linkedin
+        </Link>
+        <Link href="/" target={'_blank'}>
+          github
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
